098d08077d25fcfaea0a4afa4567d75d181cf13b
Eduardo <florianoeduardo03@gmail.com>
2022-12-21 15:46:48 -0300

    Final

---
+++ Alunos-API/src/test/java/br/com/mesttra/alunosapi/AlunosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mesttra.alunosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
